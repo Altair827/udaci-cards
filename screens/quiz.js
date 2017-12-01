0f885d0fffaa7bc6553b821e92d49e231e5cbc66
coffeecup827 <coffeecup827@gmail.com>
2017-12-02 01:49:03 +0530

    Quiz functionality added

--- screens/quiz.js
+++ screens/quiz.js
@@ -11,7 +11,12 @@ class Quiz extends React.Component {
     super(props);
     this.state = {
       deck : this.props.navigation.state.params.deck,
-      isQuestionVisible : true
+      isQuestionVisible : true,
+      currentQuestion : null,
+      score : 0,
+      currentQuestionCount : 0,
+      isQuestionsReady : false,
+      isQuizEnd : false
     }
     this.props.FetchAllQuestions(this.state.deck.key);
   }
@@ -20,26 +25,75 @@ class Quiz extends React.Component {
     title: 'Quiz'
   }
 
+  componentWillReceiveProps(newProps){
+    if(typeof newProps.questions !== null && typeof this.state.currentQuestion !== null){
+      this.setState({
+        currentQuestion : newProps.questions[0],
+        currentQuestionCount : 1,
+        isQuestionsReady : true,
+        isQuizEnd : false
+      })
+    }
+  }
+
+  answer = (bool) => {
+
+    if(this.state.currentQuestionCount !== this.state.deck.QuestionsCount){
+
+      this.setState((prevState) => {
+        return {
+          currentQuestionCount: prevState.currentQuestionCount + 1,
+          currentQuestion : this.props.questions[prevState.currentQuestionCount],
+          score : (bool === prevState.currentQuestion.IsCorrect) ? prevState.score + 1 : prevState.score
+        };
+      });
+
+    }
+    else if(this.state.currentQuestionCount === this.state.deck.QuestionsCount) {
+      this.setState((prevState) => {
+        return {
+          currentQuestionCount: 0,
+          currentQuestion : null,
+          score : (bool === prevState.currentQuestion.IsCorrect) ? prevState.score + 1 : prevState.score,
+          isQuestionsReady : false,
+          isQuizEnd : true
+        };
+      });
+    }
+
+  }
+
+  retry = () => {
+    this.setState({
+      currentQuestion : this.props.questions[0],
+      currentQuestionCount : 1,
+      isQuestionsReady : true,
+      isQuizEnd : false,
+      score : 0
+    })
+  }
+
   render() {
 
-    const questionsRetrived = this.props.questions !== null;
+    const questionsRetrived = this.state.isQuestionsReady && !this.state.isQuizEnd;
 
     return (
       <View style={styles.mainContainer}>
 
-        { questionsRetrived ?
+        { questionsRetrived &&
 
           <View style={styles.mainContainer}>
 
             <View style={styles.questionTrackerContainer}>
-              <Text style={styles.questionTrackerText}>{'1/' + this.state.deck.QuestionsCount}</Text>
+              <Text style={styles.questionTrackerText}>{this.state.currentQuestionCount + '/' + this.state.deck.QuestionsCount}</Text>
             </View>
 
             <View style={styles.cardContainer}>
 
               <View style={[styles.QuestionContainer,{display : this.state.isQuestionVisible ? 'flex' : 'none'}]}>
+                <Text style={styles.helpText}>Question</Text>
                 <View style={styles.cardTextContainer}>
-                  <Text style={styles.cardText}>{this.props.questions[0].Question}</Text>
+                  <Text style={styles.cardText}>{this.state.currentQuestion.Question}</Text>
                 </View>
                 <View style={styles.flipContainer}>
                   <Text onPress={() => this.setState({isQuestionVisible : false})} style={styles.flipButton}>Show Answer</Text>
@@ -47,8 +101,9 @@ class Quiz extends React.Component {
               </View>
 
               <View style={[styles.AnswerContainer,{display : this.state.isQuestionVisible ? 'none' : 'flex'}]}>
+                <Text style={styles.helpText}>Answer</Text>
                 <View style={styles.cardTextContainer}>
-                  <Text style={styles.cardText}>{this.props.questions[0].Answer}</Text>
+                  <Text style={styles.cardText}>{this.state.currentQuestion.Answer}</Text>
                 </View>
                 <View style={styles.flipContainer}>
                   <Text onPress={() => this.setState({isQuestionVisible : true})} style={styles.flipButton}>Go Back</Text>
@@ -59,14 +114,14 @@ class Quiz extends React.Component {
 
             <View style={styles.controlConatiner}>
               <CustomizableButton
-                onPress={() => {}}
+                onPress={() => this.answer(true)}
                 buttonStyle={[styles.button,styles.correctButton]}
                 textStyle={styles.correctText}
                 text="Correct"
               />
 
               <CustomizableButton
-                onPress={() => {}}
+                onPress={() => this.answer(false)}
                 buttonStyle={[styles.button,styles.incorrectButton]}
                 textStyle={styles.incorrectText}
                 text="Incorrect"
@@ -74,7 +129,11 @@ class Quiz extends React.Component {
             </View>
           </View>
 
-          :
+        }
+
+        {
+
+          !this.state.isQuestionsReady && !this.state.isQuizEnd &&
 
           <ActivityIndicator
                animating={!questionsRetrived}
@@ -82,6 +141,32 @@ class Quiz extends React.Component {
                size="large"
                style={styles.loader}
           />
+
+        }
+
+        {
+          this.state.isQuizEnd &&
+
+            <View style={styles.scoreContainer}>
+
+              <Text style={{fontSize : 24, margin : 15}}>Your Score Is</Text>
+              <Text style={{fontSize : 40, color : '#6200EA', margin : 30}}>{this.state.score / this.state.deck.QuestionsCount * 100}%</Text>
+
+              <CustomizableButton
+                onPress={() => this.retry()}
+                buttonStyle={[styles.button,{backgroundColor : '#1E88E5'}]}
+                textStyle={styles.buttonText}
+                text="Retry Quiz"
+              />
+
+              <CustomizableButton
+                onPress={() => this.props.navigation.goBack()}
+                buttonStyle={[styles.button,{backgroundColor : '#388E3C'}]}
+                textStyle={styles.buttonText}
+                text="Back To Deck"
+              />
+
+            </View>
 
         }
 
@@ -170,6 +255,25 @@ const styles = StyleSheet.create({
   },
   AnswerContainer : {
     flex : 1
+  },
+  scoreContainer : {
+    flex : 1,
+    justifyContent : 'center',
+    alignItems : 'center',
+    elevation : 2,
+    borderRadius : 4,
+    margin : 5
+  },
+  buttonText : {
+    color : 'white',
+    fontSize : 24
+  },
+  helpText : {
+    fontSize : 16,
+    top : 10,
+    left : 10,
+    alignSelf : 'flex-start',
+    color : '#009688'
   }
 })
 
